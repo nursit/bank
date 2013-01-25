378be602a0051bd1e9abaa6846044f4eea2e93d4
Matthieu Marcillaud <marcimat@rezo.net>
2013-01-25 19:34:08 +0100

    Traduction en anglais (cym)

---
+++ lang/bank_en.php
@@ -0,0 +1,49 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+'titre_menu_configurer' => 'Online payments',
+'titre_page_configurer_paiement' => 'Online payments',
+'titre_menu_transactions' => 'Transactions',
+'explication_page_configurer_paiement' => 'Set up active payment systems',
+
+'label_mode_paiement' => 'Transaction payment modes',
+'label_mode_paiement_abo' => 'Subscriptions payment modes',
+'label_presta_ogone' => 'Ogone <a href="http://www.ogone.fr/">http://www.ogone.fr/</a>',
+'label_presta_paybox' => 'Paybox <a href="http://www.paybox.com/">http://www.paybox.com/</a>',
+'label_presta_paypal' => 'Non secure Paypal <a href="http://www.paypal.fr/">http://www.paypal.fr/</a>',
+'label_presta_paypal_express' => 'Paypal Express Checkout <a href="http://www.paypal.fr/">http://www.paypal.fr/</a>',
+'label_presta_sips' => 'SIPS',
+'label_presta_wha' => 'Internet +',
+'label_presta_cheque' => 'Checks (manual check remittance)',
+'label_presta_simu' => 'Simulation (requires authorisation by define)',
+'label_presta_cmcic' => 'CMCIC <a href="https://www.cmcicpaiement.fr/fr/">cmcicpaiement.fr</a>',
+
+'label_actif' => 'Activate',
+'label_inactif' => 'Disable',
+
+'label_notifications' => 'Notifications',
+'label_email_ticket_admin' => 'Administrator\'s email address for reception of transaction tickets',
+
+
+'carte_bleu'=>'Credit Card',
+'choisissez_cb'=>'Select your Credit Card:',
+'abonnement_par_CB' => 'with my Credit Card',
+'payer' => 'Pay',
+'payer_avec' => 'Pay with @nom@',
+
+
+'titre_reglement_ok'=>'Successfull payment',
+'confirme_reglement_pris_en_compte' => 'Your payment has been taken into account, thank you.',
+'titre_reglement_annule'=>'Cancellation',
+'confirme_reglement_annule' => 'The operation was cancelled. No payment has been achieved',
+
+'info_1_transaction' => '1 transaction',
+'info_nb_transactions' => '@nb@ transactions',
+
+);
+
+
+?>
