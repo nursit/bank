8bb48aa6bc855031181f3d3394c1b46bb98de0e3
Cedric <cedric@yterium.com>
2013-03-28 18:04:38 +0100

    permettre de definir le chemin de l'exec paybox par un define

--- presta/paybox/exec_request.php
+++ presta/paybox/exec_request.php
@@ -13,7 +13,11 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 
 function presta_paybox_exec_request_dist($params,$request='modulev2.cgi'){
-	$path_bin = _DIR_PLUGIN_BANK . "presta/paybox/bin/$request";
+	if (defined('_DIR_PAYBOX_EXEC'))
+		$path_bin = _DIR_PAYBOX_EXEC;
+	else
+		$path_bin = _DIR_PLUGIN_BANK . "presta/paybox/bin/";
+	$path_bin .= $request;
 	if (!file_exists($path_bin)){
 		spip_log("Binaire $path_bin non trouve","paybox."._LOG_ERREUR);
 		return false;
