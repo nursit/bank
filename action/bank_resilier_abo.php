02d2d9fa7bf027d2c32893f625e2f05b3152aaf9
Cerdic <cedric@yterium.com>
2014-02-25 12:19:53 +0100

    une action pour resilier un abo depuis le site : se charge d'appeler l'API abos/resilier qui elle appelera le presta/call/resilier_abonnement

---
+++ action/bank_resilier_abo.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Paiement Bancaire
+ * module de paiement bancaire multi prestataires
+ * stockage des transactions
+ *
+ * Auteurs :
+ * Cedric Morin, Nursit.com
+ * (c) 2012 - Distribue sous licence GNU/GPL
+ *
+ */
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+/**
+ * Resiliation d'un abo quand c'est le site qui est maitre :
+ * On declenche abos/resilier de l'API abos si elle existe
+ * charge a elle de declencher prest/xxx/call/resilier_abonnement si elle existe
+ *
+ * @param null|string $uid
+ */
+function action_bank_resilier_abo_dist($uid=null){
+	if (is_null($uid)){
+		$securiser_action = charger_fonction('securiser_action','inc');
+		$uid = $securiser_action();
+	}
+
+	if ($resilier = charger_fonction('resilier','abos',true)){
+		$resilier("uid:$uid");
+	}
+}
