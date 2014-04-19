7b0b954d9c1aaea91e0be11a1cd59c859b588df8
Cedric <cedric@yterium.com>
2014-04-19 12:33:56 +0200

    Chaines de langue : rangement et mise a jour de la trad en

--- lang/bank_fr.php
+++ lang/bank_fr.php
@@ -4,75 +4,68 @@
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-'titre_menu_configurer' => 'Paiements en ligne',
-'titre_page_configurer_paiement' => 'Paiements en ligne',
-'titre_menu_transactions' => 'Transactions',
-'explication_page_configurer_paiement' => 'Configurez les syst&egrave;mes de paiement actifs',
+	'abonnement_avec' => 'Abonnement par <i>@nom@</i>',
+	'abonnement_choisissez_CB' => 'Choisissez votre carte bancaire&nbsp;:',
+	'abonnement_par_CB' => 'avec ma carte bancaire',
 
-'label_mode_paiement' => 'Modes de paiements a l\'acte',
-'label_mode_paiement_abo' => 'Modes de paiements des abonnements',
-'label_mode_test' => 'Utiliser en mode TEST (aucun paiement réel)',
+	'bouton_enregistrer_reglement_cheque' => 'Enregistrer le r&eacute;glement par ch&egrave;que',
 
-'label_presta_abo_paybox' => 'Paybox <a href="http://www.paybox.com/">http://www.paybox.com/</a>',
-'label_presta_abo_simu' => 'Simulation (necessite une autorisation par define)',
-'label_presta_abo_internetplus' => 'Internet +',
+	'carte_bleu' => 'Carte Bleue',
+	'choisissez_cb' => 'Choisissez votre carte bancaire&nbsp;:',
+	'confirme_reglement_annule' => 'L\'op&eacute;ration a &eacute;t&eacute; annul&eacute;e. Aucun r&egrave;glement n\'a &eacute;t&eacute; r&eacute;alis&eacute;',
+	'confirme_reglement_pris_en_compte' => 'Votre r&egrave;glement a bien &eacute;t&eacute; pris en compte, et nous vous en remercions.',
 
+	'explication_page_configurer_paiement' => 'Configurez les syst&egrave;mes de paiement actifs',
 
-'label_presta_ogone' => 'Ogone <a href="http://www.ogone.fr/">http://www.ogone.fr/</a>',
-'label_presta_paybox' => 'Paybox <a href="http://www.paybox.com/">http://www.paybox.com/</a>',
-'label_presta_paypal' => 'Paypal non s&eacute;curis&eacute; <a href="http://www.paypal.fr/">http://www.paypal.fr/</a>',
-'label_presta_paypalexpress' => 'Paypal Express Checkout <a href="http://www.paypal.fr/">http://www.paypal.fr/</a>',
-'label_presta_sips' => 'SIPS',
-'label_presta_internetplus' => 'Internet +',
-'label_presta_cyberplus' => 'CyberPlus',
-'label_presta_cheque' => 'Ch&egrave;ques (encaissement manuel)',
-'label_presta_simu' => 'Simulation (necessite une autorisation par define)',
-'label_presta_cmcic' => 'CMCIC <a href="https://www.cmcicpaiement.fr/fr/">cmcicpaiement.fr</a>',
-'label_presta_cmcic_banque' => 'Banque',
-'label_presta_sips_service' => 'Service',
-
-'label_actif' => 'Activer',
-'label_inactif' => 'D&eacute;sactiver',
-
-'label_notifications' => 'Notifications',
-'label_email_ticket_admin' => 'Email pour l\'envoi des tickets d\'achat admin',
-
-'label_configuration_ordre' => 'Ordre',
-'label_configuration_adresse' => 'Adresse où envoyer les chèques',
-'label_cheque_notice' => 'Information complémentaire affichée',
-'legend_sips_logo_page_paiement' => 'Logos page de paiement',
-
-'info_cheque_imprimer' => 'Envoyez votre chèque d&#8217;un montant de <b>@montant@&nbsp;€</b> avec la référence &laquo; <b>Commande #@transaction@</b> &raquo;<br /> à l&#8217;adresse :',
-'info_cheque_etablir_ordre' => 'Chèque établi à l\'ordre de &laquo;&nbsp;<i>@ordre@</i>&nbsp;&raquo;',
-'info_cheque_facture' => 'La facture sera disponible dans votre espace client dès votre règlement pris en compte.',
-'info_mode_test' => 'Mode TEST (paiement fictif)',
-'bouton_enregistrer_reglement_cheque' => 'Enregistrer le réglement par chèque',
+	'info_1_transaction' => '1 transaction',
+	'info_nb_transactions' => '@nb@ transactions',
+	'info_cheque_etablir_ordre' => 'Ch&egrave;que &eacute;tabli &agrave; l\'ordre de &laquo;&nbsp;<i>@ordre@</i>&nbsp;&raquo;',
+	'info_cheque_imprimer' => 'Envoyez votre ch&egrave;que d&#8217;un montant de <b>@montant@&nbsp;€</b> avec la r&eacute;f&eacute;rence &laquo;&nbsp;<b>Commande #@transaction@</b>&nbsp;&raquo;<br /> &agrave; l&#8217;adresse :',
+	'info_cheque_facture' => 'La facture sera disponible dans votre espace client d&egrave;s votre r&egrave;glement pris en compte.',
+	'info_mode_test' => 'Mode TEST (paiement fictif)',
 
 
-'carte_bleu'=>'Carte Bleue',
-'choisissez_cb'=>'Choisissez votre carte bancaire&nbsp;:',
-'abonnement_par_CB' => 'avec ma carte bancaire',
-'payer' => 'Payer',
-'payer_avec' => 'Payer avec <i>@nom@</i>',
-'abonnement_avec' => 'Abonnement par <i>@nom@</i>',
-'abonnement_choisissez_CB' => 'Choisissez votre carte bancaire&nbsp;:',
+	'label_actif' => 'Activer',
+	'label_cheque_notice' => 'Information compl&eacute;mentaire affich&eacute;e',
+	'label_configuration_ordre' => 'Ordre',
+	'label_configuration_adresse' => 'Adresse o&ugrave; envoyer les ch&egrave;ques',
+	'label_email_ticket_admin' => 'Email pour l\'envoi des tickets d\'achat admin',
+	'label_inactif' => 'D&eacute;sactiver',
+	'label_mode_paiement' => 'Modes de paiements a l\'acte',
+	'label_mode_paiement_abo' => 'Modes de paiements des abonnements',
+	'label_mode_test' => 'Utiliser en mode TEST (aucun paiement r&eacute;el)',
+	'label_notifications' => 'Notifications',
+	'label_presta_abo_paybox' => 'Paybox <a href="http://www.paybox.com/">http://www.paybox.com/</a>',
+	'label_presta_abo_simu' => 'Simulation (necessite une autorisation par define)',
+	'label_presta_abo_internetplus' => 'Internet +',
+	'label_presta_cheque' => 'Ch&egrave;ques (encaissement manuel)',
+	'label_presta_cmcic' => 'CMCIC <a href="https://www.cmcicpaiement.fr/fr/">cmcicpaiement.fr</a>',
+	'label_presta_cmcic_banque' => 'Banque',
+	'label_presta_cyberplus' => 'CyberPlus',
+	'label_presta_internetplus' => 'Internet +',
+	'label_presta_ogone' => 'Ogone <a href="http://www.ogone.fr/">http://www.ogone.fr/</a>',
+	'label_presta_paybox' => 'Paybox <a href="http://www.paybox.com/">http://www.paybox.com/</a>',
+	'label_presta_paypal' => 'Paypal non s&eacute;curis&eacute; <a href="http://www.paypal.fr/">http://www.paypal.fr/</a>',
+	'label_presta_paypalexpress' => 'Paypal Express Checkout <a href="http://www.paypal.fr/">http://www.paypal.fr/</a>',
+	'label_presta_simu' => 'Simulation (necessite une autorisation par define)',
+	'label_presta_sips' => 'SIPS',
+	'label_presta_sips_service' => 'Service',
+	'label_resilier_abonnement' => 'R&eacute;silier',
 
-'payer_par_cheque' => 'Payer par chèque :',
-'payer_par_carte_bancaire' => 'Payer par carte bancaire :',
+
+	'legend_sips_logo_page_paiement' => 'Logos page de paiement',
+
+	'payer' => 'Payer',
+	'payer_avec' => 'Payer avec <i>@nom@</i>',
+	'payer_par_cheque' => 'Payer par ch&egrave;que :',
+	'payer_par_carte_bancaire' => 'Payer par carte bancaire :',
 
 
-'titre_reglement_ok'=>'R&egrave;glement r&eacute;ussi',
-'confirme_reglement_pris_en_compte' => 'Votre r&egrave;glement a bien &eacute;t&eacute; pris en compte, et nous vous en remercions.',
-'titre_reglement_annule'=>'Annulation',
-'confirme_reglement_annule' => 'L\'op&eacute;ration a &eacute;t&eacute; annul&eacute;e. Aucun r&egrave;glement n\'a &eacute;t&eacute; r&eacute;alis&eacute;',
-
-'info_1_transaction' => '1 transaction',
-'info_nb_transactions' => '@nb@ transactions',
-
-'label_resilier_abonnement' => 'Résilier',
-
+	'titre_reglement_ok' => 'R&egrave;glement r&eacute;ussi',
+	'titre_menu_configurer' => 'Paiements en ligne',
+	'titre_page_configurer_paiement' => 'Paiements en ligne',
+	'titre_menu_transactions' => 'Transactions',
+	'titre_reglement_annule' => 'Annulation',
 
 );
 
-
-?>
