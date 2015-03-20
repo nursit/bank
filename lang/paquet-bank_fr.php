0c9f5741254ae4d4be9d31327fe34af3ba85752b
Cerdic <cedric@yterium.com>
2015-03-20 15:22:30 +0100

    Debut refactoring plugin, on reformalise en abandonnant la compat SPIP 2

---
+++ lang/paquet-bank_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+///  Fichier produit par PlugOnet
+// Module: paquet-bank
+// Langue: fr
+// Date: 20-03-2015 15:07:32
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'bank_description' => 'Un module de paiement bancaire multi prestataires',
+	'bank_slogan' => 'Un module de paiement bancaire multi prestataires',
+);
+?>
