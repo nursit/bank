0a8c4b7d093dddaf6fdca8f18fb0973038aa8acd
Cerdic <cedric@yterium.com>
2015-03-24 15:22:35 +0100

    toutes les infos du paiement par cheque viennent de la config

--- presta/cheque/payer/acte.php
+++ presta/cheque/payer/acte.php
@@ -13,13 +13,17 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 function presta_cheque_payer_acte_dist($id_transaction,$transaction_hash){
 
+	include_spip("inc/bank");
+	$config = bank_config("cheque");
+
 	return recuperer_fond(
 		'presta/cheque/payer/acte',
 		array(
-			'action' => generer_url_action('bank_response', 'bankp=cheque',true,true),
+			'action' => bank_url_api_retour($config,"response"),
 			'id_transaction' => $id_transaction,
 			'transaction_hash' => $transaction_hash,
 			'attente_mode' => _request('attente_mode'),
+			'config' => $config,
 		),
 		array(
 			'ajax'=>true
