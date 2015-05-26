710cd5871e627376864e45571b665dacf57eb296
Cerdic <cedric@yterium.com>
2015-05-26 16:26:15 +0200

    code inutile, tout est verifie dans paypal_traite_response

--- presta/paypal/call/response.php
+++ presta/paypal/call/response.php
@@ -28,34 +28,6 @@ function presta_paypal_call_response($config, $response=null){
 		foreach ($_POST as $key => $value){
 			$response[$key] = $value;
 		}
-		// si invoice dans les post
-		if (isset($response['invoice'])){
-			list($response['id_transaction'],$response['transaction_hash']) = explode('|',$response['invoice']);
-		}
-		// sinon id_transaction et hash en get ?
-		else {
-			$response['id_transaction'] = _request('id_transaction');
-			$response['transaction_hash'] = _request('transaction_hash');
-		}
-	}
-
-	if (!isset($response['id_transaction']) OR !isset($response['transaction_hash'])){
-		spip_log("id_transaction ou transaction_hash absent ".var_export($response,true),$mode._LOG_ERREUR);
-		return array(0,false);
-	}
-
-	$id_transaction = $response['id_transaction'];
-	$transaction_hash = $response['transaction_hash'];
-
-	if (!$id_transaction
-		OR !$row = sql_fetsel("*","spip_transactions", "id_transaction=".intval($id_transaction)." AND transaction_hash=".sql_quote($transaction_hash))) {
-		spip_log("Check:Transaction $id_transaction inconnue",$mode._LOG_ERREUR);
-		return array(0,false);
-	}
-
-	if ($row['statut']=='ok') {
-		spip_log("Check:Transaction $id_transaction deja validee",$mode);
-		return array($id_transaction,true);
 	}
 
 	include_spip('presta/paypal/inc/paypal');
