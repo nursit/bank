c145dbcb3411b930dec55527dfcbf9454277a23e
RastaPopoulos <rastapopoulos@spip.org>
2020-03-30 17:49:25 +0200

    Utiliser la bonne division suivant la devise dans la REPONSE de systempay aussi

--- presta/systempay/inc/systempay.php
+++ presta/systempay/inc/systempay.php
@@ -378,7 +378,8 @@ function systempay_traite_reponse_transaction($config, $response){
 	if ($is_payment){
 		// Ouf, le reglement a ete accepte
 		// on verifie que le montant est bon !
-		$montant_regle = $response['vads_effective_amount']/100;
+		$devise_defaut = bank_devise_defaut();
+		$montant_regle = $response['vads_effective_amount'] / (10**$devise_defaut['fraction']);
 		if ($montant_regle!=$row['montant']){
 			spip_log($t = "call_response : id_transaction $id_transaction, montant regle $montant_regle!=" . $row['montant'] . ":" . bank_shell_args($response), $mode);
 			// on log ca dans un journal dedie
