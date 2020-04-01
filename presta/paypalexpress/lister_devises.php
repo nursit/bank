c3a8b5d16a3638001ce61c2d896767a44c281a18
RastaPopoulos <rastapopoulos@spip.org>
2020-04-01 19:19:12 +0200

    Implémentation de la liste des devises supportées pour Paypal Express

---
+++ presta/paypalexpress/lister_devises.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function presta_paypalexpress_lister_devises_dist() {
+	return array('AUD','BRL','CAD','CZK','DKK','EUR','HKD','HUF','INR','ILS','JPY','MYR','MXN','TWD','NZD','NOK','PHP','PLN','GBP','RUB','SGD','SEK','CHF','THB','USD');
+}
