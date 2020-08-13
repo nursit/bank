82487c6bf719525ddef2b5b7244d96064388383c
Cerdic <cedric@yterium.com>
2020-08-13 14:00:46 +0200

    Deplacement de l'implementation de reference dans payzen/

--- presta/systempay/config.php
+++ presta/systempay/config.php
@@ -15,44 +15,12 @@ if (!defined('_ECRIRE_INC_VERSION')){
 
 /* Systempay  ----------------------------------------------------------- */
 
-
-/**
- * Constantes pour Systempay
- *
- * Vous pouvez définir ces constantes dans votre fichier mes_options.php
- *
- * Il vous faudra obtenir 3 informations de Systempay et les définir dans
- * les constantes correspondantes :
- * - Identifiant Boutique à récupérer dans le Back office de la solution de paiement (constante _SYSTEMPAY_ID),
- * - Certificat à récupérer dans le Back office de la solution de paiement (constante _SYSTEMPAY_CLE)
- *
- * Le fonctionnement est le suivant :
- * - le visiteur de votre site clique le bouton de paiement Systempay, il est redirigé sur la banque
- * - là, il peut effectuer 3 actions :
- *   1) annuler : il retourne sur votre site (ce qui annule sa transaction)
- *   2) payer correctement : il reste sur le site de la banque,
- *      MAIS celle-ci a dialoguée avec votre site qui a pris en compte la transaction,
- *      (La transaction passe en statut «ok» et réglée).
- *      Il peut alors retourner sur votre site via le lien de retour OK,
- *      ou quitter la page, peut importe.
- *   3) rater son paiement : il reste sur le site de la banque,
- *      MAIS celle-ci a dialoguée avec votre site qui prend en compte l'erreur
- *      (la transaction passe en statut «echec»). Il peut soit retenter son paiement,
- *      ce qui revient au point 2) ou 3), soit annuler, ce qui revient au point 1)
- *
- */
-
-# Version du logiciel
-if (!defined('_SYSTEMPAY_VERSION')){
-	define("_SYSTEMPAY_VERSION", "V2");
-}
-
 function systempay_lister_cartes_config($c){
 	$config = array(
 		'presta' => 'systempay',
 		'type' => isset($c['type']) ? $c['type'] : 'acte',
 		'service' => isset($c['service']) ? $c['service'] : 'cyberplus'
 	);
-	include_spip("presta/systempay/inc/systempay");
-	return systempay_available_cards($config);
+	include_spip("presta/payzen/inc/payzen");
+	return payzen_available_cards($config);
 }
