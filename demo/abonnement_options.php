8d1c3191e82e0ac73ed941c7a77ae4a4df450d31
Cerdic <cedric@yterium.com>
2021-05-25 11:23:32 +0200

    une fonction abos_preparer_echeance() de test/debug pour tester les abonnements

--- demo/abonnement_options.php
+++ demo/abonnement_options.php
@@ -21,3 +21,42 @@ if (!function_exists('abos_decrire_echeance')) {
 	        return $desc;
 	}
 }
+if (!function_exists('abos_preparer_echeance')){
+	function abos_preparer_echeance($id){
+
+		spip_log("abos/preparer_echeance id=$id", "bank" . _LOG_DEBUG);
+
+		$where = "0=1";
+		if (strncmp($id,"uid:",4)==0){
+			$where = "abo_uid=".sql_quote(substr($id,4));
+		}
+
+		// retrouver la transaction d'origine de l'abonnement
+		if ($transaction = sql_fetsel('*', 'spip_transactions', "reglee='oui' AND $where", '', 'date_transaction', '0,1')) {
+
+
+			$montant = $transaction['montant'];
+			$options = [
+				'montant_ht' => $transaction['montant_ht'],
+				'devise' => $transaction['devise'],
+				'id_auteur' => $transaction['id_auteur'],
+				'auteur_id' => $transaction['auteur_id'],
+				'auteur' => $transaction['auteur'],
+				'parrain' => $transaction['parrain'],
+				'tracking_id' => $transaction['tracking_id'],
+				'champs' => [
+					'abo_uid' => $id,
+				]
+			];
+
+			$inserer_transaction = charger_fonction('inserer_transaction', 'bank');
+			if ($id_transaction = $inserer_transaction($montant, $options)){
+				spip_log("abos/preparer_echeance id=$id transaction $id_transaction ajoutee", "bank" . _LOG_DEBUG);
+				return $id_transaction;
+			}
+		}
+
+
+		return false;
+	}
+}
