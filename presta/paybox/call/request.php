b2fabe240efdb015a45e45463c1979b9bb522a94
cy_altern <cy.altern@gmail.com>
2021-10-13 14:33:46 +0200

    comme pour d'autres prestataires, utiliser la fonction bank_porteur_infos_facturation() pour récupérer l'email afin de pouvoir utiliser le pipeline bank_dsp2_renseigner_facturation pour gérer le mail envoyé

--- presta/paybox/call/request.php
+++ presta/paybox/call/request.php
@@ -65,7 +65,8 @@ function presta_paybox_call_request_dist($id_transaction, $transaction_hash, $co
 		);
 	}
 
-	$mail = bank_porteur_email($row);
+	$billing = bank_porteur_infos_facturation($row);
+	$mail = $billing['email'];
 
 	// passage en centimes d'euros : round en raison des approximations de calcul de PHP
 	$montant = intval(round((10**$devise_info['fraction']) * $row['montant'], 0));
