2273b78c092b7e5d77ec53fe380c73b55291414f
cy_altern <cy.altern@gmail.com>
2022-04-01 00:46:10 +0200

    éviter un warning en PHP 8 (presta: chèque)

--- presta/cheque/lister_devises.php
+++ presta/cheque/lister_devises.php
@@ -15,7 +15,7 @@ if (!defined('_ECRIRE_INC_VERSION')){
 
 function presta_cheque_lister_devises_dist($config) {
 	// Si ce mode par chèque a une config de devise, c'est uniquement ça qui est accepté
-	if ($config['devise']) {
+	if (isset($config['devise']) and $config['devise']) {
 		return array($config['devise']);
 	}
 	
